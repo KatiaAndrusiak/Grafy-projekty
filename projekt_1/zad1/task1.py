a035e979c4a3d4e4e55235bb5dbb65caad3f2f80
Vadym <vsamkovich@ukr.net>
2021-03-24 16:55:05 +0100

    fixed 1 task

--- projekt_1/zad1/task1.py
+++ projekt_1/zad1/task1.py
@@ -2,54 +2,56 @@ import sys
 import convert_functions as cf
 import print_functions as pf
 
-    
-
 adj_task_matrix = []
 adj_task_list = []
 inc_task_matrix = []
 
+if (sys.argv[1] == "--am"):
+    adj_task_matrix = cf.read_matrix_from_file(sys.argv[2], adj_task_matrix)
+    #AM
+    pf.print_matrix(adj_task_matrix)
+    
+    #AM -> AL
+    adj_list = cf.convert_adj_matrix_to_adj_list(adj_task_matrix)
+    pf.print_adj_list(adj_list)
 
-adj_task_matrix = cf.read_matrix_from_file("macierzA.txt", adj_task_matrix)
-adj_task_list = cf.read_matrix_from_file("listaA.txt", adj_task_list)
-inc_task_matrix = cf.read_matrix_from_file("macierzI.txt", inc_task_matrix)
-##################TASK 1.################## 
+    #AM -> IM
+    inc_matrix = cf.convert_adj_matrix_to_inc_matrix(adj_task_matrix)
+    pf.print_matrix(inc_matrix, "Macierz incydencji")
+elif (sys.argv[1] == "--al"):
+    adj_task_list = cf.read_matrix_from_file(sys.argv[2], adj_task_list)
+    #AL
+    pf.print_adj_list(adj_task_list)
 
-#AM
-pf.print_matrix(adj_task_matrix)
+    #AL -> AM
+    adj_matrix = cf.convert_adj_list_to_adj_matrix(adj_task_list)
+    pf.print_matrix(adj_matrix)
 
-#AM -> AL
-adj_list = cf.convert_adj_matrix_to_adj_list(adj_task_matrix)
-pf.print_adj_list(adj_list)
+    #AL -> IM
+    inc = cf.convert_adj_list_to_inc_matrix(adj_task_list)
+    pf.print_matrix(inc, "Macierz incydencji")
+elif (sys.argv[1] == '--im'):
+    inc_task_matrix = cf.read_matrix_from_file(sys.argv[2], inc_task_matrix)  
+    #IM
+    pf.print_matrix(inc_task_matrix, "Macierz incydencji")
 
-#AM -> IM
-inc_matrix = cf.convert_adj_matrix_to_inc_matrix(adj_task_matrix)
-pf.print_matrix(inc_matrix, "Macierz incydencji")
-########################################### 
+    #IM -> AM
+    adjm = cf.convert_inc_matrix_to_adj_matrix(inc_task_matrix)
+    pf.print_matrix(adjm)
 
-#AL
-pf.print_adj_list(adj_task_list)
-
-#AL -> AM
-adj_matrix = cf.convert_adj_list_to_adj_matrix(adj_task_list)
-pf.print_matrix(adj_matrix)
-
-#AL -> IM
-inc = cf.convert_adj_list_to_inc_matrix(adj_task_list)
-pf.print_matrix(inc, "Macierz incydencji")
-########################################### 
-
-#IM
-pf.print_matrix(inc_task_matrix, "Macierz incydencji")
-
-#IM -> AM
-adjm = cf.convert_inc_matrix_to_adj_matrix(inc_task_matrix)
-pf.print_matrix(adjm)
-
-#IM -> AL
-adjl = cf.convert_inc_matrix_to_adj_list(inc_task_matrix)
-pf.print_adj_list(adjl)
+    #IM -> AL
+    adjl = cf.convert_inc_matrix_to_adj_list(inc_task_matrix)
+    pf.print_adj_list(adjl)
 
 
+#Aby uruchomić kod trzeba podać flagę oraz scieżkę do pliku, np:
+# python task1.py --am ./nazwa_pliku
+# --am -  macierz sąsiedztwa
+# --al -  lista sąsiedztwa
+# --im -  macierz incydencji
+
+##############################################################
+# W kazdym if nalezy jeszcze dodać rysowanie grafu (Zadanie 2)
 
 
 
