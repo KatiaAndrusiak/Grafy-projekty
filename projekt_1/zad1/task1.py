9f006c26ca8ccb2d57e215a7d35e6b51d0308ec0
Bartosz Piętka <57501043+barpje@users.noreply.github.com>
2021-03-27 01:29:15 +0100

    add zad2

--- projekt_1/zad1/task1.py
+++ projekt_1/zad1/task1.py
@@ -1,6 +1,7 @@
 import sys
 import convert_functions as cf
 import print_functions as pf
+import plot_functions as plt
 
 adj_task_matrix = []
 adj_task_list = []
@@ -14,10 +15,11 @@ if (sys.argv[1] == "--am"):
     #AM -> AL
     adj_list = cf.convert_adj_matrix_to_adj_list(adj_task_matrix)
     pf.print_adj_list(adj_list)
-
     #AM -> IM
     inc_matrix = cf.convert_adj_matrix_to_inc_matrix(adj_task_matrix)
     pf.print_matrix(inc_matrix, "Macierz incydencji")
+    #plot
+    plt.plotCircleGraph(adj_list)
 elif (sys.argv[1] == "--al"):
     adj_task_list = cf.read_matrix_from_file(sys.argv[2], adj_task_list)
     #AL
@@ -30,6 +32,8 @@ elif (sys.argv[1] == "--al"):
     #AL -> IM
     inc = cf.convert_adj_list_to_inc_matrix(adj_task_list)
     pf.print_matrix(inc, "Macierz incydencji")
+    #plot
+    plt.plotCircleGraph(adj_task_list)
 elif (sys.argv[1] == '--im'):
     inc_task_matrix = cf.read_matrix_from_file(sys.argv[2], inc_task_matrix)  
     #IM
@@ -42,16 +46,19 @@ elif (sys.argv[1] == '--im'):
     #IM -> AL
     adjl = cf.convert_inc_matrix_to_adj_list(inc_task_matrix)
     pf.print_adj_list(adjl)
+    #plot
+    plt.plotCircleGraph(adjl)
 
-
-#Aby uruchomić kod trzeba podać flagę oraz scieżkę do pliku, np:
+# Przed pierwszym uruchomieniem:
+# pip install -r requirements.txt
+# Aby uruchomić kod trzeba podać flagę oraz scieżkę do pliku, np:
 # python task1.py --am ./nazwa_pliku
 # --am -  macierz sąsiedztwa
 # --al -  lista sąsiedztwa
 # --im -  macierz incydencji
 
 ##############################################################
-# W kazdym if nalezy jeszcze dodać rysowanie grafu (Zadanie 2)
+# (Zadanie 2) Wykresy generowane na podstawie listy sasiedztwa
 
 
 
