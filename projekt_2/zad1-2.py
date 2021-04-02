984a1401dec0096deb88e316fff02309e678cdbd
Kateryna Andrusiak <katia.andru@gmail.com>
2021-04-02 22:54:09 +0200

    fixed task1 and task2 project2

--- projekt_2/zad1-2.py
+++ projekt_2/zad1-2.py
@@ -1,11 +1,14 @@
+import sys
+import os.path
 import random
 from operator import itemgetter
-
 from typing import List
-
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import print_functions as pf
 from utils import convert_functions as cf
 from utils import plot_functions as plot
+
+
 
 def degree_sequence_check(c) -> bool:
     print(' '.join(map(str, c)))
@@ -32,9 +35,10 @@ def read_list_from_file(file_name, mylist) -> object:
     return mylist
 
 
-def convert_ds_to_am(deg_seq) -> object:
+def convert_ds_to_am(deg_seq) -> List[List[int]]:
     indexed_deg_seq = [[index, degree] for index, degree in enumerate(deg_seq)]
     mat = [[0] * len(deg_seq) for i in range(len(deg_seq))]
+
     while True:
         indexed_deg_seq = sorted(indexed_deg_seq, key=itemgetter(1), reverse=True)
         if indexed_deg_seq[0][1] == 0:
@@ -47,8 +51,8 @@ def convert_ds_to_am(deg_seq) -> object:
     return mat
 
 
-def randomize_edges(am, n):
-    rand_matrix = am
+def randomize_edges(am: List[List[int]], n):
+    rand_matrix = am.copy()
     pf.print_matrix(rand_matrix)
     edges_list: List[List[int]] = []
 
@@ -56,8 +60,10 @@ def randomize_edges(am, n):
         for j in range(i+1, len(rand_matrix[0])):
             if rand_matrix[i][j] == 1:
                 edges_list.append([i, j])
+    print(edges_list)
 
     for i in range(n):
+        n = 1000
         while True:
             ab = edges_list[random.randint(0, len(edges_list) - 1)]
             cd = edges_list[random.randint(0, len(edges_list) - 1)]
@@ -65,11 +71,13 @@ def randomize_edges(am, n):
             bd = [ab[1], cd[1]]
             ac_reversed = [ac[1], ac[0]]
             bd_reversed = [bd[1], bd[0]]
-
             if ab != cd and ab[0] != cd[0] and ab[1] != cd[1]  \
                     and ac not in edges_list and bd not in edges_list \
                     and ac_reversed not in edges_list and bd_reversed not in edges_list:
                 break
+            n -= 1
+            if n == 0:
+                return am
 
         rand_matrix[ab[0]][ab[1]] = 0
         rand_matrix[ab[1]][ab[0]] = 0
@@ -92,28 +100,27 @@ def randomize_edges(am, n):
     return rand_matrix
 
 
-
-
-
-
-
 if __name__ == '__main__':
     degree_sequence = []
-    degree_sequence = read_list_from_file("degreeSeq.txt", degree_sequence)
+    degree_sequence = read_list_from_file(sys.argv[1], degree_sequence)
 
     degree_sequence = list(map(int, degree_sequence))
 
     if degree_sequence_check(degree_sequence):
         print("\nCiąg jest graficzny")
-        adjacency_matrix = convert_ds_to_am(degree_sequence)
-        pf.print_adj_list(adjacency_matrix)
-        adjacency_list = cf.convert_adj_matrix_to_adj_list(adjacency_matrix)
-        pf.print_adj_list(adjacency_list)
-        rand_mat = randomize_edges(adjacency_matrix, 10)
-        pf.print_matrix(rand_mat)
-        rand_list = cf.convert_adj_matrix_to_adj_list(rand_mat)
-        plot.plotCircleGraph(rand_list)
+        adjacency_matrix: List[List[int]] = convert_ds_to_am(degree_sequence)
+        pf.print_matrix(adjacency_matrix)
 
+        rand_mat = randomize_edges(adjacency_matrix, 100)
+
+        rand_list = cf.convert_adj_matrix_to_adj_list(rand_mat)
+        pf.print_matrix(rand_mat)
+        pf.print_adj_list(rand_list)
+        plot.plotCircleGraph(rand_list)
 
     else:
         print("\nCiąg nie jest graficzny")
+
+
+# Aby uruchomić kod trzeba podać nazwę pliku, np:
+# python zad1-2.py ./nazwa_pliku
